23eafcc9496be67a560f6a098dfe03c127020fad
João Pedro Gonçalves <joao.araujo@sou.inteli.edu.br>
2025-02-03 21:35:51 -0300

    thrd commit

--- peixe.js
+++ peixe.js
@@ -15,6 +15,8 @@ var tubaraozin;
 var peixinho;
 let pontos = 0;
 let TextoPontos;
+let tv = 3;
+let nivel = 1;
 
 function preload() {
     this.load.image('mar', 'assets/bg_azul-escuro.png')
@@ -26,7 +28,11 @@ function preload() {
 function create() {
     this.add.image(400, 300, 'mar')
     this.add.image(400, 525, 'logo').setScale(0.5);
-    TextoPontos = this.add.text(25, 25, "Pontos: 0", {
+    TextoPontos = this.add.text(25, 75, "Pontos: 0", {
+        fontSize: '32px',
+        fill: '#ffffff' 
+    });
+    nivelText = this.add.text(25, 25, `Level: ${nivel}`, {
         fontSize: '32px',
         fill: '#ffffff' 
     });
@@ -34,6 +40,11 @@ function create() {
     this.time.addEvent({
         delay: 1000,
         callback: aumentarPontos,
+        loop: true
+    });
+    this.time.addEvent({
+        delay: 15000,
+        callback: LevelUp,
         loop: true
     });
 
@@ -54,7 +65,7 @@ function update() {
         peixinho.x = this.input.x;
         peixinho.y = this.input.y;
 
-        let tv = 15;
+        
         tubaraozin.x = tubaraozin.x + tv;
 
         if (tubaraozin.x > 800) {
@@ -66,6 +77,7 @@ function update() {
 
         if (Phaser.Geom.Intersects.RectangleToRectangle(peixinho.getBounds(), tubaraozin.getBounds())) {
            zerarPontos();
+           zerarLevel();
         }
 
     }
@@ -77,5 +89,15 @@ function aumentarPontos() {
 function zerarPontos() {
     pontos = 0;
     TextoPontos.setText(`Pontos: ${pontos}`);
+}
+function LevelUp() {
+    nivel += 1;
+    nivelText.setText(`Level: ${nivel}`);
+    tv = tv * 2
+}
+function zerarLevel() {
+    nivel = 1;
+    nivelText.setText(`Level: ${nivel}`);
+    tv = 2;
 }
 
